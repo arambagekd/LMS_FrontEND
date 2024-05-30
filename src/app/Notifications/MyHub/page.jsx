d872c51f15164f1f1ba7162302dc700df5d25282
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-05-30 14:48:40 +0530

    correct to vercel

--- src/app/Notifications/MyHub/page.jsx
+++ src/app/Notifications/MyHub/page.jsx
@@ -3,7 +3,7 @@ import React, { Children, useEffect, useState } from "react";
 import {
   HubConnectionBuilder,
   LogLevel,
-} from "@microsft/signalr";
+} from "@microsoft/signalr";
 import TextArea from "antd/es/input/TextArea";
 import { Button, message, notification } from "antd";
 import { duration } from "moment";

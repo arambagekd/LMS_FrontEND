f2c1aab09b73d96840c62c84d1e3c8df97585c0b
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-06-03 10:38:49 +0530

    nmg

--- src/app/LogIN/ForgetPassword/Component/LoginCard.jsx
+++ src/app/LogIN/ForgetPassword/Component/LoginCard.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginForm from './Loginform';
 import Image from 'next/image';
 import { Spin } from 'antd';

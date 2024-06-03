0854981f9e7cc034ed08200bd298b433f836aa9c
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-06-03 15:52:32 +0530

    nav

--- src/app/Component/Navigations.jsx
+++ src/app/Component/Navigations.jsx
@@ -10,8 +10,12 @@ import {
   InteractionOutlined,
   InfoCircleOutlined,
   DashboardOutlined,
+  NotificationOutlined,
+  MessageTwoTone,
+  MessageFilled,
   SettingOutlined,
   EditOutlined,
+  QuestionCircleOutlined,
 } from "@ant-design/icons";
 import {
   Button,
@@ -20,15 +24,23 @@ import {
   Flex,
   Layout,
   Menu,
+  Space,
+  theme,
   Avatar,
-  Spin,
+  Badge,
   Divider,
+  Spin,
+  Result,
 } from "antd";
 import Link from "next/link";
 import AdressBar from "./AdressBar";
+import MenuItem from "antd/es/menu/MenuItem";
 import { usePathname, useRouter } from "next/navigation";
+import { image1 } from "../../../public/lib1.jpg";
 import Image from "next/image";
+
 import axios from "axios";
+
 import Cookies from "js-cookie";
 import Profile from "./Profile";
 import axioinstance from "../Instance/api_instance";
@@ -36,8 +48,7 @@ import { UserContext, EmailContext } from "../Context/Context";
 import MyHub from "../Notifications/MyHub/page";
 import ErrorPage from "../ErrorPage/page";
 import NotificationDrawer from "./NotificationDrawer";
-import styles from "./Navigations.module.css";
-
+import { get } from "http";
 const { Header, Content, Footer, Sider } = Layout;
 
 const sideitems = [
@@ -53,6 +64,7 @@ const sideitems = [
   },
   {
     key: "Users",
+
     icon: React.createElement(UserOutlined),
     label: <Link href="/Users">Users</Link>,
   },
@@ -94,16 +106,19 @@ const sideitems2 = [
     icon: React.createElement(ReadOutlined),
     label: <Link href="/Resources">Resources</Link>,
   },
+
   {
     key: "Requests",
     icon: React.createElement(AuditOutlined),
     label: <Link href="/Requests">Requests</Link>,
   },
+
   {
     key: "Reservations",
     icon: React.createElement(InteractionOutlined),
     label: <Link href="/Reservations">Reservations</Link>,
   },
+
   {
     key: "Settings",
     icon: React.createElement(SettingOutlined),
@@ -116,6 +131,7 @@ function Navigations(props) {
   const [user, setUser] = useState({});
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(true);
+  const [Notifications, setNotification] = useState([]);
   const [open, setOpen] = useState(false);
   const [authenticated, setAuthenticated] = useState(false);
 
@@ -126,30 +142,35 @@ function Navigations(props) {
         { withCredentials: true }
       );
       Cookies.remove("jwt");
+      //  const response=axios.post();
+      console.log(response);
       router.push("/LogIN");
     } catch (error) {
       console.log(error);
     }
   };
 
-  async function GetUser() {
+    async function GetUser(){
+     
     try {
       const response = await axioinstance.post("User/GetMyData");
       const response1 = await axioinstance.post("User/GetEmail");
+      console.log(response.data);
       setUser(response.data);
       setEmail(response1.data);
     } catch (error) {
+      
       console.log(error);
       setLoading(false);
     }
-  }
-
+  };
   const selectPatron = async (usertype) => {
     try {
       const token = Cookies.get("jwt");
       Cookies.remove("jwt");
+      console.log(token);
       const response = await axios.post(
-        `https://ac89-212-104-231-166.ngrok-free.app/api/Auth/selectusertype?userType=${usertype}`,
+        `https://fb10-61-245-161-144.ngrok-free.app/api/Auth/selectusertype?userType=${usertype}`,
         null,
         {
           withCredentials: true,
@@ -167,25 +188,33 @@ function Navigations(props) {
   };
 
   const [collapsed, setCollapsed] = useState(false);
+  // const {
+  //   token: { colorBgContainer, borderRadiusLG },
+  // } = theme.useToken();
+
   const location = usePathname();
+
+  // Extracting the root path
   const rootPath = location.split("/")[1];
 
   const items = [
     {
       key: "1",
+
       label: (
         <Avatar onClick={() => setOpen(true)} icon={<MessageOutlined />} />
       ),
     },
     {
       key: "2",
+
       label: <Avatar icon={<UserOutlined />} />,
       children: [
         {
           label: (
             <a href="https://www.antgroup.com">
               <center>
-                <Avatar icon={<UserOutlined />} />
+                <Avatar icon={<UserOutlined />} />{" "}
               </center>
             </a>
           ),
@@ -208,6 +237,11 @@ function Navigations(props) {
           label: <a href="https://www.aliyun.com">Settings </a>,
           key: "5",
         },
+        // {
+        //   icon: React.createElement(QuestionCircleOutlined),
+        //   label: <a href="https://www.aliyun.com">Help & Support </a>,
+        //   key: "6",
+        // },
         {
           icon: React.createElement(InfoCircleOutlined),
           label: <a href="https://www.aliyun.com">About</a>,
@@ -225,9 +259,9 @@ function Navigations(props) {
     },
   ];
 
-  useEffect(() => {
-    GetUser();
-  }, []);
+    useEffect(() => {
+      GetUser();
+    }, []);
 
   useEffect(() => {
     if (user.userName != undefined) {
@@ -244,8 +278,9 @@ function Navigations(props) {
         ) : !authenticated ? (
           <ErrorPage />
         ) : (
-          <Layout className={styles.layout}>
+          <Layout style={{ minHeight: "100vh" }}>
             <NotificationDrawer open={open} setOpen={setOpen} />
+
             <Sider
               collapsible
               collapsed={collapsed}
@@ -253,7 +288,14 @@ function Navigations(props) {
               style={{ height: "auto" }}
             >
               <div style={{ position: "sticky", top: 0 }}>
-                <div className={styles.siderLogo}>
+                <div
+                  style={{
+                    color: "white",
+                    width: "100%",
+                    textAlign: "center",
+                    padding: "20px 10px",
+                  }}
+                >
                   {!collapsed ? (
                     <Image src="/translib.png" width={140} height={35} />
                   ) : (
@@ -283,11 +325,15 @@ function Navigations(props) {
               </div>
             </Sider>
             <Layout>
-              <Header style={{zIndex: 2,
-                  position: 'sticky',
+              <Header
+                style={{
+                  zIndex: 2,
+                  position: "sticky",
                   top: 0,
                   padding: 0,
-                  background: 'rgb(255, 255, 255)'}}>
+                  background: "rgb(255,255,255)",
+                }}
+              >
                 <ConfigProvider
                   theme={{
                     components: {
@@ -303,26 +349,27 @@ function Navigations(props) {
                     }
                     align="center"
                   >
-                    {user.actualType == "admin" && (
+                    {user.actualType == "admin" ? (
                       <>
-                        {user.userType == "admin" && (
+                        {user.userType == "admin" ? (
                           <Button
                             onClick={() => selectPatron("patron")}
                             style={{ margin: "0 0 0 15px" }}
                           >
                             Patron View
                           </Button>
-                        )}
-                        {user.userType == "patron" && (
+                        ) : null}
+                        {user.userType == "patron" ? (
                           <Button
                             onClick={() => selectPatron("admin")}
                             style={{ margin: "0 0 0 15px" }}
                           >
                             Admin View
                           </Button>
-                        )}
+                        ) : null}
                       </>
-                    )}
+                    ) : null}
+
                     <Menu
                       triggerSubMenuAction="click"
                       style={{
@@ -338,13 +385,17 @@ function Navigations(props) {
                   </Flex>
                 </ConfigProvider>
               </Header>
-              <Content className={styles.content}>
+              <Content style={{ margin: "24px 5%" }}>
                 <Card>
-                  <Flex justify="space-between" align="center" wrap="">
-                    <Flex className={styles.title} align="center">
+                  <Flex al justify="space-between" align="center" wrap="">
+                    <Flex
+                      style={{ fontSize: "25px", fontWeight: "600" }}
+                      align="center"
+                    >
+                      {" "}
                       <Button
                         onClick={() => router.back()}
-                        className={styles.backButton}
+                        style={{ margin: "0 20px 0 0" }}
                         shape="circle"
                         icon={<ArrowLeftOutlined />}
                       />
@@ -355,12 +406,13 @@ function Navigations(props) {
                     </div>
                   </Flex>
                   <Divider />
-                  <Flex className={styles.addressBarContainer}>
+                  <Flex a vertical style={{ margin: "10px 0 0 0 " }}>
                     <MyHub>{props.children}</MyHub>
                   </Flex>
                 </Card>
               </Content>
-              <Footer className={styles.footer}>
+
+              <Footer style={{ textAlign: "center" }}>
                 Ant Design ©{new Date().getFullYear()} Created by Ant UED
               </Footer>
             </Layout>

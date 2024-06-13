7ce558f649b6507271aefc63ece4f1d64253187d
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-06-13 10:29:25 +0530

    Update api_instance.js

--- src/app/Instance/api_instance.js
+++ src/app/Instance/api_instance.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 //const token =Cookies.get('jwt');
 const axioinstance = axios.create(
   {
-  baseURL : 'https://7978-61-245-171-62.ngrok-free.app/',
+  baseURL : 'https://7978-61-245-171-62.ngrok-free.app/api/',
   headers: {
     //'Authorization': `Bearer ${token}`,
     'Content-Type': "application/json",

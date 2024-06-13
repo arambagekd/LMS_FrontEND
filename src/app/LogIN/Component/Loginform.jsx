262c5b3e84435407542a26dafcedd29483b2df9a
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-06-13 13:57:21 +0530

    Update Loginform.jsx

--- src/app/LogIN/Component/Loginform.jsx
+++ src/app/LogIN/Component/Loginform.jsx
@@ -71,11 +71,11 @@ function Loginform({spinning,setSpinning}) {
             //   cookies.set('jwt', jwtToken, { path: '/', httpOnly: true });
               console.log(response.data);
               console.log(response.data);
-              if(firebasetoken!="no"||firebasetoken!=undefined){
-                const response2 =await axios.post('https://7978-61-245-171-62.ngrok-free.app/api/Notification/SetFireBaseToken',{
-                  token:firebasetoken,
-                  userName:user
-                })};
+              // if(firebasetoken!="no"||firebasetoken!=undefined){
+              //   const response2 =await axios.post('https://7978-61-245-171-62.ngrok-free.app/api/Notification/SetFireBaseToken',{
+              //     token:firebasetoken,
+              //     userName:user
+              //   })};
               //redirect(`/Dashboard`)
               getUser();
               // console.log(user);

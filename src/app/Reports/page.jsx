f46fcb464f1a0623b2e1fa97c6f52baec4ee11a6
Kavindu Dilshan Arambage <157782852+arambagekd@users.noreply.github.com>
2024-07-03 07:58:36 +0530

    Update page.jsx

--- src/app/Reports/page.jsx
+++ src/app/Reports/page.jsx
@@ -1,6 +1,6 @@
 import React, { Children } from 'react'
 import { UserOutlined, MessageOutlined, ReadOutlined, AuditOutlined, InteractionOutlined, InfoCircleOutlined,DashboardOutlined,HomeOutlined  } from '@ant-design/icons';
-import Table from './Component/Table'
+import Table from './Component/table';
 
 function page() {
   return (
